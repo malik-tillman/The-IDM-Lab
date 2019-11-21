f7fa0525b2474895e5023319c48345d8f1861125
malik-tillman <contact@maliktillman.com>
2019-11-21 18:56:42 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'main-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'the-idm-lab';
+  title: string = 'Interpolated String';
+
+  
 }
